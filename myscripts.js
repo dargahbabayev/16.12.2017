98a71dfaec18cf1d729050ae7ac6c48e9a621f71
dargahbabayev <34453796+dargahbabayev@users.noreply.github.com>
2017-12-16 01:14:23 +0400

    1.Odd divisors

---
+++ myscripts.js
@@ -0,0 +1,16 @@
+var n = Number(prompt("n  reqemi girin"));
+var count=0;
+var i=1;
+if(n>0){
+    while(i<n){
+if(i%2==1){ 
+    count++
+
+}
+
+i++
+
+    } console.log("tekreqemlerin sayi= " +count);
+
+}
+else{(console.log("duzgun sayiyi girin"));}
